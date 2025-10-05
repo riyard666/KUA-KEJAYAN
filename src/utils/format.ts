7374238fbe6d77b9cbf01236c9f5e0344a8385ad
Achmad Shofyan <achshofyan.afyn@gmail.com>
2025-10-05 21:00:59 +0700

    Feat: Update format clock

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -2,11 +2,11 @@ import {format} from "date-fns";
 import { id } from "date-fns/locale";
 
 export function FormatDate(rawDate: string) {
-    return format(new Date(rawDate), "dd MMM, yyyy hh:mm a",  { locale: id });
+    return format(new Date(rawDate), "dd MMM, yyyy hh:mm",  { locale: id });
 }
 
 export function FormatDateId(rawDate: string) {
-    return format(new Date(rawDate), "dd MMMM, yyyy hh:mm a",  { locale: id });
+    return format(new Date(rawDate), "dd MMMM, yyyy hh:mm",  { locale: id });
 }
 
 export function FormatDateNoTime(rawDate?: string) {
