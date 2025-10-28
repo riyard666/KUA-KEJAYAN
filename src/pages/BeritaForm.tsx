dcadbfe9bbb6fe85dffdc89f93ef226be39969ff
Achmad Shofyan <achshofyan.afyn@gmail.com>
2025-10-28 14:53:47 +0700

    feat: BeritaForm

--- src/pages/BeritaForm.tsx
+++ src/pages/BeritaForm.tsx
@@ -1,10 +1,12 @@
-import React, { useRef, useState } from "react";
-import EditorJS from "@editorjs/editorjs";
+import React, {useRef, useState} from "react";
+import EditorJS, {type ToolConstructable} from "@editorjs/editorjs";
+import List from "@editorjs/list";
+
 import Header from "@editorjs/header";
 import Paragraph from "@editorjs/paragraph";
 import ImageTool from "@editorjs/image";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
 
 export default function BeritaForm() {
     const editorRef = useRef<EditorJS | null>(null);
@@ -22,11 +24,67 @@ export default function BeritaForm() {
 
     const initEditor = () => {
         if (!editorRef.current) {
-            const editor = new EditorJS({
+            editorRef.current = new EditorJS({
                 holder: "editorjs",
-                tools: { header: Header, paragraph: Paragraph, image: ImageTool },
+                tools: {
+                    header: {
+                        class: Header as unknown as ToolConstructable,
+                        inlineToolbar: ["link"],
+                        config: {
+                            placeholder: "Header",
+                            levels: [1, 2, 3, 4],
+                            defaultLevel: 2,
+                        },
+                    },
+                    paragraph: {
+                        class: Paragraph as unknown as ToolConstructable,
+                        inlineToolbar: true,
+                    },
+                    list: {
+                        class: List,
+                        inlineToolbar: true,
+                    },
+                    image: {
+                        class: ImageTool,
+                        config: {
+                            // uploader: {
+                            //     /**
+                            //      * Upload file gambar ke backend Laravel
+                            //      */
+                            //     async uploadByFile(file: File) {
+                            //         try {
+                            //             const formData = new FormData();
+                            //             formData.append("file", file);
+                            //
+                            //             const response = await api.post(
+                            //                 "/portfolio/upload-image",
+                            //                 formData,
+                            //                 {
+                            //                     headers: {
+                            //                         "Content-Type": "multipart/form-data",
+                            //                     },
+                            //                 }
+                            //             );
+                            //
+                            //             if (response.data.success === 1) {
+                            //                 return response.data; // { success: 1, file: { url: '...' } }
+                            //             } else {
+                            //                 throw new Error(response.data.message || "Upload gagal");
+                            //             }
+                            //         } catch (error) {
+                            //             const errorMessage = error as AxiosError<{message?: string}>;
+                            //             console.error("Upload error:", errorMessage);
+                            //             return {
+                            //                 success: 0,
+                            //                 message: errorMessage.response?.data?.message || "Upload gagal",
+                            //             };
+                            //         }
+                            //     }
+                            // }
+                        }
+                    }
+                },
             });
-            editorRef.current = editor;
         }
     };
 
